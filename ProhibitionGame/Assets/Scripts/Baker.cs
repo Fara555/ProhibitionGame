84ffe20d3cd68b4eca5e2069f82a2eae680ed3e9
Fara555 <faraway51423@gmail.com>
2024-08-01 12:57:56 +0500

    fix repo

---
+++ ProhibitionGame/Assets/Scripts/Baker.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+
+public class Baker : MonoBehaviour
+{
+	private Ingridients ingridients;
+
+	private string[] fruits = new string[5];
+
+	private void Start()
+	{
+		Bake(ingridients.apple);
+	}
+
+	private void Bake(GameObject ingridient)
+	{
+		for (int i = 0; i < fruits.Length; i++)
+		{
+			if ( == )
+			{
+
+			}
+		}
+		// Кладу предмет(ingridient) в печку
+	}
+}
